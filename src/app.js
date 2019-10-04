ee2dbe52cda82e710d39314aff2e054e425f147d
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2019-10-03 22:32:02 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.app = express();
+
+    this.middleware();
+  }
+
+  middleware() {
+    this.app.use(routes);
+  }
+}
+
+export default new App().app;
