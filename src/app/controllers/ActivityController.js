b0c1cde9cb50a7adecdde4ea39bd9fa062b5afdc
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2019-10-17 20:09:26 -0300

    Updates activity business rules

--- src/app/controllers/ActivityController.js
+++ src/app/controllers/ActivityController.js
@@ -1,8 +1,43 @@
+import {
+  startOfWeek,
+  startOfDay,
+  endOfDay,
+  isBefore,
+  isAfter,
+  parseISO,
+} from 'date-fns';
+
 import Activity from '../models/Activity';
 
 class ActivityController {
   async store(req, res) {
-    const { id, realized_date: realizedDate } = await Activity.create(req.body);
+    // eslint-disable-next-line camelcase
+    const { user_id, assignment_id } = req.body;
+    // eslint-disable-next-line camelcase
+    const realized_date = startOfDay(parseISO(req.body.realized_date));
+
+    const startDateOfWeek = startOfWeek(new Date(), { weekStartsOn: 1 });
+    if (isBefore(realized_date, startDateOfWeek))
+      return res.status(400).json({ error: 'Current week only' });
+
+    if (isAfter(realized_date, endOfDay(new Date())))
+      return res.status(400).json({ error: 'Day higher than current' });
+
+    const existentActivity = await Activity.findOne({
+      where: { user_id, assignment_id, realized_date: realized_date.toJSON() },
+    });
+
+    if (existentActivity) {
+      return res
+        .status(400)
+        .json({ error: 'Activity is already registered on this day' });
+    }
+
+    const { id, realized_date: realizedDate } = await Activity.create({
+      user_id,
+      assignment_id,
+      realized_date: realized_date.toJSON(),
+    });
 
     return res.json({ id, realizedDate });
   }
