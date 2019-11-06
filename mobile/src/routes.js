e7566179d8db334836a0679b27107e2bb3a55cc0
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2019-11-05 22:42:47 -0300

    Mobile add navigation

---
+++ mobile/src/routes.js
@@ -0,0 +1,10 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SignIn from './pages/SignIn';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+  })
+);
